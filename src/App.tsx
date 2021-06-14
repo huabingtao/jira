c0ad49d25d3cf05c725b3a603723fbfd18f44067
huabingtao <1273621932@qq.com>
2021-06-14 19:27:04 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"><img src={logo} className="App-logo" alt="logo" />{" "}<p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
