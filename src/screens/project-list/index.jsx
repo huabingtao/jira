d7560bf2c7990d1a076cad4092f675dafa823fce
huabingtao <1273621932@qq.com>
2021-06-18 17:57:19 +0800

    feat: list

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,8 +1,20 @@
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
 export const ProjectListScreen = ()=>{
+    const [list, setList] = useState([])
+    useEffect((param,setParam)=>{
+        fetch('').then(async (res)=>{
+            if(res.ok){
+                setList(await res.json())
+            }
+        })
+    },[param])
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    })
     return <div>
-        <SearchPanel></SearchPanel>
-        <List></List>
+        <SearchPanel param={param} setParam={setParam}></SearchPanel>
+        <List list={list}></List>
     </div>
 }
--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,19 +1,9 @@
 import { useEffect, useState } from "react"
 
 export const SearchPanel = ()=>{
-    const [param, setParam] = useState({
-        name: '',
-        personId: ''
-    })
+
     const [users, setUsers] = useState([])
-    const [list, setList] = useState([])
-    useEffect(()=>{
-        fetch('').then(async (res)=>{
-            if(res.ok){
-                setList(await res.json())
-            }
-        })
-    },[param])
+
     return <form action="">
         <div>
             <input type="text" value={param.name} onChange={event=>{
