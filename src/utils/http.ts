0ec064afb3f4c35b568e5548c7f05949bfddc1ed
huabingtao <1273621932@qq.com>
2021-06-23 23:34:41 +0800

    feat: 封装fetch

---
+++ src/utils/http.ts
@@ -0,0 +1,35 @@
+import * as auth from "auth-provider";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+      const data = response.json();
+      if (response.ok) {
+        return Promise.resolve(data);
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
