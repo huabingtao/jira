a9412e7a4c4ab227c992adf4c015cb7e9bff5b29
huabingtao <1273621932@qq.com>
2021-07-14 21:32:04 +0800

    style: 用row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
@@ -6,7 +7,12 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>11</h2>
+          <h2>22</h2>
+          <h3>333</h3>
+        </HeaderLeft>
         <button onClick={logout}>登出</button>
       </Header>
       <Main>
@@ -30,9 +36,9 @@ const Container = styled.div`
   grid-template-columns: 20rem 1fr 20rem; */
   height: 100vh;
 `;
-const Header = styled.header`
-  grid-area: header;
-`;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
 const Main = styled.main`
   grid-area: main;
 `;
